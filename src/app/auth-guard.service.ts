70d23348391204f644afacecbf6b64abe32aacee
Martin Slábek <martin.slabek@educanet.cz>
2021-03-12 16:12:31 +0100

    final update

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {CanActivate} from '@angular/router';
+import {CanActivate, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from './auth.service';
 import { Router } from '@angular/router';
 
@@ -9,13 +9,13 @@ export class AuthGuard implements CanActivate{
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate(): any {
+  canActivate(router, state: RouterStateSnapshot): any {
       // @ts-ignore
     return this.auth.user$.map(user => {
         if (user) {
           return true;
         } else {
-        this.router.navigate(['/login']);
+        this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
         return false;
         }
     });
